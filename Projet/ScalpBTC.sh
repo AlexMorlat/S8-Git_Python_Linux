e7946fdf2b7b7aaf06498f341f7cd538fbaf5e8c
EC2 Default User <ec2-user@ip-172-31-40-63.eu-west-3.compute.internal>
2023-03-09 21:31:13 +0000

    Write grep for VM ScalpBTC.sh

--- Projet/ScalpBTC.sh
+++ Projet/ScalpBTC.sh
@@ -1,1 +1,2 @@
-echo $(date -u)";"$(curl https://www.coingecko.com/en/coins/bitcoin | html2text | sed -n '/Bitcoin Price (BTC)/,$p' | sed -n '/Share on Facebook/q;p' | sed -n '3p' | grep -oP '\$\d{1,3}(,\d{3})*\.\d{2}') >> /home/alexandre/PrixBTC.csv
+#!/usr/bin/env bash
+echo $(date -u)";"$(curl -s https://www.coingecko.com/en/coins/bitcoin | grep '<span class="no-wrap" data-price-btc="1.0" data-coin-id="1" data-coin-symbol="btc" data-target="price.price">' | grep -oP '\$\d{1,3}(,\d{3})*\.\d{2}' | sed -n '1p') >> /home/ec2-user/S8-Git_Python_Linux/Projet/PrixBTC.csv
